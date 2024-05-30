660e4b1a10f72a2084679f0ebcd427c65806b80d
Roberto Vetere <Roberto.vetere@gmail.com>
2024-05-30 11:39:42 +0200

    first commit

---
+++ src/main/java/com/hlidevs/store_products/StoreProductsApplication.java
@@ -0,0 +1,13 @@
+package com.hlidevs.store_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hlidevs/store_products/StoreProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlidevs.store_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
